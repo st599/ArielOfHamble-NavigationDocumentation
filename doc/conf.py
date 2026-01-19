de53bc1a888df0d066ceede3ce4adf4e1a86d540
st599 <st599@users.noreply.github.com>
2026-01-19 15:13:24 +0000

    Update project metadata in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'Ariel of Hamble Navigation Documentation'
+copyright = 'BBCYC'
+author = 'Simon Thompson'
 release = '0.1'
 
 
