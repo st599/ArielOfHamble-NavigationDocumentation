3652ed23bae656c73a002765d25dba25c1c01dfd
st599 <st599@users.noreply.github.com>
2026-01-19 17:34:30 +0000

    Update HTML theme to 'breeze' in conf.py
    Changed HTML theme from 'sphinx_rtd_theme' to 'breeze' with updated options.

--- doc/conf.py
+++ doc/conf.py
@@ -24,5 +24,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+
+html_theme = 'breeze'
+html_theme_options = {'header_tabs': False}
+
 html_static_path = ['_static']
