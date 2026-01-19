aa14d2d90ae2b58f2634c866d1fa34750194ef11
st599 <st599@users.noreply.github.com>
2026-01-19 17:39:51 +0000

    Update HTML theme in Sphinx configuration
    Changed the HTML theme from 'breeze' to 'piccolo_theme'.

--- doc/conf.py
+++ doc/conf.py
@@ -25,8 +25,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'sphinx_rtd_theme'
-
-html_theme = 'breeze'
-html_theme_options = {'header_tabs': False}
+html_theme = 'piccolo_theme'
 
 html_static_path = ['_static']
