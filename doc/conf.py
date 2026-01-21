a9fb1c01b1becbc870a939fb4dc7dee1bbc2b7bd
st599 <st599@users.noreply.github.com>
2026-01-21 15:25:10 +0000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinx.ext.mathjax', 'sphinx.builders.epub3', 'sphinxcontrib-htmlhelp', 'sphinxcontrib-serializinghtml']
+extensions = ['myst_parser', 'sphinx.ext.mathjax', 'sphinx.builders.epub3']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
