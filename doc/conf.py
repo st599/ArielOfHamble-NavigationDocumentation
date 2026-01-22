94f818cc40c5b7d03a7612ec7ec5f11f102b57d2
st599 <st599@users.noreply.github.com>
2026-01-22 09:34:49 +0000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -27,3 +27,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'piccolo_theme'
 
 html_static_path = ['_static']
+
+# -- Options for PDF output -------------------------------------------------
+# https://sphinx-simplepdf.readthedocs.io/en/latest/configuration.html
+
+simplepdf_file_name = "AoH-Navigation.pdf"
+
