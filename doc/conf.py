dc7cb10e6742d7a208d58c7975157e1deb74f339
st599 <st599@users.noreply.github.com>
2026-01-24 16:30:53 +0000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = 'Ariel of Hamble Navigation'
 copyright = 'BBCYC 2026'
 author = 'Simon Thompson'
-version = '0.2'
+version = '0.3'
 release = version
 
 # -- General configuration ---------------------------------------------------
